b7c7462ea211fd346a125fd6362cbbad678419db
Alexis <alexis.morin.mail@gmail.com>
2021-03-08 21:51:46 -0500

    reflection probes

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Generator : MonoBehaviour
-{
+public class Generator : MonoBehaviour {
     [Header("Generation Settings")]
 
     public AnimationCurve tensionMap;
@@ -34,13 +33,11 @@ public class Generator : MonoBehaviour
     public int currentRoomCount = 1;
     public List<Room> rooms = new List<Room>();
 
-    void Start()
-    {
+    void Start() {
         Init();
     }
 
-    void Init()
-    {
+    void Init() {
         GameObject startRoom = Instantiate(startRooms[Random.Range(0, startRooms.Length)], Vector3.zero, Quaternion.identity);
         startRoom.transform.parent = this.transform;
         roomBounds.Add(startRoom.GetComponent<MeshRenderer>().bounds);
@@ -51,23 +48,19 @@ public class Generator : MonoBehaviour
 
     }
 
-    public void ResetQA()
-    {
+    public void ResetQA() {
         CancelInvoke("StartQA");
         Invoke("StartQA", 0.2f);
     }
 
-    void StartQA()
-    {
+    void StartQA() {
         StartCoroutine(QA());
     }
 
-    IEnumerator QA()
-    {
+    IEnumerator QA() {
         yield return null;
 
-        if (currentRoomCount < minRooms)
-        {
+        if (currentRoomCount < minRooms) {
 
             Debug.LogWarning("too small");
             ResetWorld();
@@ -75,8 +68,7 @@ public class Generator : MonoBehaviour
         }
 
         GameObject[] endRooms = GameObject.FindGameObjectsWithTag("GoalRoom");
-        if (endRooms.Length == 0)
-        {
+        if (endRooms.Length == 0) {
             Debug.LogWarning("no end room");
             ResetWorld();
             yield break;
@@ -86,16 +78,14 @@ public class Generator : MonoBehaviour
 
         GameObject furthestRoom = endRooms[0];
         float goldenPathDistance = 0;
-        for (int i = 0; i < endRooms.Length; i++)
-        {
+        for (int i = 0; i < endRooms.Length; i++) {
             NavMeshPath currentPath = new NavMeshPath();
             NavMesh.CalculatePath(transform.position, furthestRoom.transform.position, NavMesh.AllAreas, currentPath);
 
             NavMeshPath testPath = new NavMeshPath();
             NavMesh.CalculatePath(transform.position, endRooms[i].transform.position, NavMesh.AllAreas, testPath);
 
-            if (PathDistance(testPath) > PathDistance(currentPath))
-            {
+            if (PathDistance(testPath) > PathDistance(currentPath)) {
                 furthestRoom = endRooms[i];
                 goldenPathDistance = PathDistance(testPath);
             }
@@ -103,8 +93,7 @@ public class Generator : MonoBehaviour
 
 
         // We check if the path is proper length
-        if (goldenPathDistance < minPathDistance || goldenPathDistance > maxPathDistance)
-        {
+        if (goldenPathDistance < minPathDistance || goldenPathDistance > maxPathDistance) {
             Debug.LogWarning("unsuitablepath");
             ResetWorld();
             yield break;
@@ -120,16 +109,13 @@ public class Generator : MonoBehaviour
         Process(furthestRoom);
     }
 
-    void Process(GameObject furthestRoom)
-    {
+    void Process(GameObject furthestRoom) {
         List<Room> roomTensionTemp = new List<Room>();
 
         Transform currentSelection = furthestRoom.transform;
-        while (currentSelection.transform.parent != null)
-        {
+        while (currentSelection.transform.parent != null) {
             Room room;
-            if (currentSelection.TryGetComponent<Room>(out room))
-            {
+            if (currentSelection.TryGetComponent<Room>(out room)) {
                 room.isCritical = true;
                 roomTensionTemp.Add(room);
             }
@@ -137,39 +123,30 @@ public class Generator : MonoBehaviour
         }
 
         // We transfer tension data onto the room data
-        for (int i = 0; i < roomTensionTemp.Count; i++)
-        {
+        for (int i = 0; i < roomTensionTemp.Count; i++) {
             roomTensionTemp[i].tension = tensionMap.Evaluate(1f - (1f / roomTensionTemp.Count * i));
         }
 
 
         // we go along each room. if it's critical, we wind down it's outputs, trimming bifurcations when they become too long.
-        for (int i = 0; i < rooms.Count; i++)
-        {
+        for (int i = 0; i < rooms.Count; i++) {
             Room room = rooms[i];
-            if (room.isCritical && !room.isBifurcation)
-            {
-                foreach (Transform child in room.transform)
-                {
+            if (room.isCritical && !room.isBifurcation) {
+                foreach (Transform child in room.transform) {
                     // look for outputs in the room
-                    if (child.name == "DoorFrame")
-                    {
+                    if (child.name == "DoorFrame") {
                         // we found a bifurcation. loop down and trim as needed!
-                        if (child.GetChild(1).GetComponent<Room>().isCritical == false)
-                        {
+                        if (child.GetChild(1).GetComponent<Room>().isCritical == false) {
                             int bifurcation = 0;
                             GameObject bifurcatedRoom = child.GetChild(1).gameObject;
 
-                            while (bifurcation < bifurcationDepth)
-                            {
+                            while (bifurcation < bifurcationDepth) {
                                 bifurcatedRoom.name = System.DateTime.Now.Millisecond.ToString();
                                 print(bifurcatedRoom.name);
                                 bifurcatedRoom.GetComponent<Room>().isCritical = true;
                                 bifurcatedRoom.GetComponent<Room>().isBifurcation = true;
-                                foreach (Transform bifurcatedChild in bifurcatedRoom.transform)
-                                {
-                                    if (bifurcatedChild.name == "DoorFrame")
-                                    {
+                                foreach (Transform bifurcatedChild in bifurcatedRoom.transform) {
+                                    if (bifurcatedChild.name == "DoorFrame") {
                                         bifurcatedRoom = bifurcatedChild.GetChild(1).gameObject;
                                     }
                                 }
@@ -181,23 +158,37 @@ public class Generator : MonoBehaviour
             }
         }
 
-        for (int i = 0; i < rooms.Count; i++)
-        {
+        // Delete non critical rooms
+        // Cap off open-ended doors
+        for (int i = 0; i < rooms.Count; i++) {
             Room room = rooms[i];
-            if (room.isCritical == false)
-            {
-                if (room.transform.parent.parent.GetComponent<Room>().isCritical)
-                {
+            if (room.isCritical == false) {
+                if (room.transform.parent.parent.GetComponent<Room>().isCritical) {
                     Destroy(room.gameObject);
                 }
             }
         }
+        RemoveMissingRooms();
+
+        for (int i = 0; i < rooms.Count; i++) {
+            Room room = rooms[i];
+            if (room.isCritical == true) {
+                foreach (Transform child in room.transform) {
+                    if (child.name == "DoorFrame") {
+                        if (child.childCount == 1) {
+                            SpawnCapObject(child);
+                        }
+                    }
+                }
+            }
+        }
+
+
 
         Invoke("Furnish", 0.1f);
     }
 
-    void Furnish()
-    {
+    void Furnish() {
         rooms[0].GetComponent<NavMeshSurface>().BuildNavMesh();
         RemoveMissingRooms();
     }
@@ -206,8 +197,7 @@ public class Generator : MonoBehaviour
 
     // Helpers
 
-    void ResetWorld()
-    {
+    void ResetWorld() {
         CancelInvoke("StartQA");
         StopCoroutine(QA());
         Destroy(transform.GetChild(0).gameObject);
@@ -217,34 +207,33 @@ public class Generator : MonoBehaviour
         Init();
     }
 
-    void DrawGoldenPath(GameObject furthestRoom)
-    {
+    void DrawGoldenPath(GameObject furthestRoom) {
         NavMeshPath goldenPath = new NavMeshPath();
         NavMesh.CalculatePath(transform.position, furthestRoom.transform.position, NavMesh.AllAreas, goldenPath);
 
-        for (int i = 0; i < goldenPath.corners.Length - 1; i++)
-        {
+        for (int i = 0; i < goldenPath.corners.Length - 1; i++) {
             Debug.DrawLine(goldenPath.corners[i], goldenPath.corners[i + 1], Color.yellow, 100f);
         }
 
     }
 
-    float PathDistance(NavMeshPath path)
-    {
+    float PathDistance(NavMeshPath path) {
         float distance = 0f;
-        for (int i = 0; i < path.corners.Length - 1; i++)
-        {
+        for (int i = 0; i < path.corners.Length - 1; i++) {
             distance += Vector3.Distance(path.corners[i], path.corners[i + 1]);
         }
         return distance;
     }
 
-    public void RemoveMissingRooms()
-    {
-        for (int i = rooms.Count - 1; i >= 0; i--)
-        {
-            if (rooms[i] == null)
-            {
+    void SpawnCapObject(Transform deleteableDoor) {
+        GameObject cap = Instantiate(capRooms[Random.Range(0, capRooms.Length)], deleteableDoor.position, deleteableDoor.rotation);
+        cap.transform.parent = this.transform.parent;
+        Destroy(deleteableDoor.gameObject);
+    }
+
+    public void RemoveMissingRooms() {
+        for (int i = rooms.Count - 1; i >= 0; i--) {
+            if (rooms[i] == null) {
                 rooms.RemoveAt(i);
                 roomBounds.RemoveAt(i);
             }
---
+++ Assets/Scripts/PlaceReflectionProbe.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceReflectionProbe : MonoBehaviour {
+
+    public bool refresh = false;
+    // Start is called before the first frame update
+    void OnValidate() {
+        print("Refreshed");
+        transform.position = transform.parent.GetComponent<MeshRenderer>().bounds.center;
+        ReflectionProbe reflectionProbe = GetComponent<ReflectionProbe>();
+        reflectionProbe.mode = UnityEngine.Rendering.ReflectionProbeMode.Realtime;
+        reflectionProbe.boxProjection = true;
+        reflectionProbe.resolution = 32;
+        reflectionProbe.blendDistance = 0.1f;
+
+        reflectionProbe.size = transform.parent.GetComponent<MeshRenderer>().bounds.size * 1.1f;
+        refresh = false;
+    }
+
+    void Start() {
+        Invoke("Bake", 1f);
+    }
+
+    void Bake() {
+        GetComponent<ReflectionProbe>().RenderProbe();
+    }
+
+}
