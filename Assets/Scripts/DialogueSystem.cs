01e95b289a1d8f980c3ae73cd2d22dd5d9377559
Alexis <alexis.morin.mail@gmail.com>
2021-03-22 03:11:43 -0400

    actual dialogue system that works
    go figure that doing it yourself works best

---
+++ Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Dialogue;
+using XNode;
+using System.Linq;
+
+public class DialogueSystem : MonoBehaviour
+{
+
+    [SerializeField]
+    DialogueGraph testdialogue;
+
+    [Header("Rendering")]
+
+    [SerializeField]
+    GameObject templateButton = null;
+
+    [SerializeField]
+    GameObject textBox;
+    Text text;
+
+    // Internals
+
+    DialogueGraph graph;
+    bool inDialogue;
+    List<GameObject> currentButtons = new List<GameObject>();
+
+    void Start()
+    {
+        text = textBox.transform.GetChild(0).GetComponent<Text>();
+
+        textBox.SetActive(false);
+        templateButton.SetActive(false);
+
+        ProcessDialogue(testdialogue);
+    }
+
+    void ProcessDialogue(DialogueGraph conversation)
+    {
+        graph = conversation;
+        graph.Restart();
+        inDialogue = true;
+
+        UpdateDialogue();
+    }
+
+    void UpdateDialogue()
+    {
+        textBox.SetActive(true);
+        text.text = graph.current.text;
+
+        // Remove old buttons
+        for (int i = 0; i < currentButtons.Count; i++)
+        {
+            Destroy(currentButtons[i]);
+        }
+
+        // Add new buttons
+        for (int i = 0; i < graph.current.answers.Count; i++)
+        {
+            GameObject buttonInstance = Instantiate(templateButton, templateButton.transform.position, Quaternion.identity, templateButton.transform.parent);
+            currentButtons.Add(buttonInstance);
+
+            buttonInstance.SetActive(true);
+            buttonInstance.transform.GetChild(0).GetComponent<Text>().text = graph.current.answers[i].text;
+            int currentAnswer = i;
+            buttonInstance.GetComponent<Button>().onClick.AddListener(() => UpdateDialogueState(currentAnswer));
+        }
+    }
+
+    public void UpdateDialogueState(int reply)
+    {
+        graph.AnswerQuestion(reply);
+        UpdateDialogue();
+    }
+
+    public void Update()
+    {
+        if (inDialogue)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+
+                if (graph.current.endConversation)
+                {
+                    inDialogue = false;
+                }
+                else
+                {
+
+                    if (graph.current.answers.Count == 0)
+                    {
+                        UpdateDialogueState(0);
+                    }
+                }
+
+            }
+        }
+    }
+
+
+}
